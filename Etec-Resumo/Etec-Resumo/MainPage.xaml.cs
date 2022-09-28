f397ad139c69ad853d56ef75fafda773111aa0ba
Tox <renatoserrano.am@gmail.com>
2022-09-28 15:18:19 -0300

    Test

--- Etec-Resumo/Etec-Resumo/MainPage.xaml.cs
+++ Etec-Resumo/Etec-Resumo/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using AppNovoGuiaCursoDS.views;
+using Etec-Resumo.Views;
 
 namespace Etec_Resumo
 {
---
+++ Etec-Resumo/Etec-Resumo/Models/Componente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Etec_Resumo.Models;
+
+namespace Etec_Resumo.Models
+{
+    public string Nome { get; set; }
+    public int CargaHoraria { get; set; }
+    public string AtribuicoesResponsabilidade { get; set; }
+    public string ValoresAtitudes { get; set; }
+
+    public string Competencias { get; set; }
+    public string Habilidades { get; set; }
+    public string BasesTecnologicas { get; set; }
+
+
+
+}
