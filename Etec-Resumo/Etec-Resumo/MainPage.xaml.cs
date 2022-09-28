50d0cb8a1fc1bfeb4795f7fcca6a38a910802b12
Usuario <Usuario@CPD03-M13>
2022-09-28 14:47:14 -0300

    Adicionar arquivos de projeto.

---
+++ Etec-Resumo/Etec-Resumo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppNovoGuiaCursoDS.views;
+
+namespace Etec_Resumo
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Inicial(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
